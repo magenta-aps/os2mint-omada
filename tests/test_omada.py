5704ab2f18b13610899eedb572d2c61e0e17d053
Casper V. Kristensen <vk@magenta.dk>
2021-11-17 21:08:47 +0100

    HYPOTHESIS :D

--- tests/test_omada.py
+++ tests/test_omada.py
@@ -1,71 +1,37 @@
 # SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 import pytest
-from respx import MockRouter
+import respx
+from hypothesis import given
+from hypothesis import strategies as st
+from hypothesis.strategies import composite
 
 from os2mint_omada import omada
 from os2mint_omada.config import settings
-from os2mint_omada.omada import OmadaITUser
 
 
-@pytest.fixture
-def mock_omada_it_users(respx_mock: MockRouter) -> None:
-    data = {
-        "value": [
-            {
-                "Id": 1006456,
-                "UId": "719b0b48-f651-40df-b980-f1d79123aa87",
-                "CreateTime": "2018-11-05T10:40:16.447+01:00",
-                "ChangeTime": "2021-06-02T08:42:45.327+02:00",
-                "C_TJENESTENR": "00185",
-                "C_OBJECTGUID_I_AD": "9ff2beb8-2283-410f-babc-70d3ec2afa0c",
-                "EMAIL": "KristofferMoller.Jensen@silkeborg.dk",
-                "EMAIL2": None,
-                "C_DIREKTE_TLF": "",
-                "CELLPHONE": "",
-                "C_INST_PHONE": "",
-                "C_LOGIN": "",
-            },
-            {
-                "Id": 1006453,
-                "UId": "f6327a33-07e3-468c-b7bb-cb852f25e3a4",
-                "CreateTime": "2018-11-05T10:40:15.273+01:00",
-                "ChangeTime": "2021-06-02T08:42:45.263+02:00",
-                "C_TJENESTENR": "00187",
-                "C_OBJECTGUID_I_AD": "1106d3a6-a624-43b5-bdb8-66b746a2a60d",
-                "EMAIL": "Gitte.Willumsen@silkeborg.dk",
-                "C_DIREKTE_TLF": "89701891",
-                "CELLPHONE": "25306035",
-                "C_INST_PHONE": "89701000",
-                "C_LOGIN": "DR00187",
-            },
-        ]
+@composite
+def it_users(draw) -> list[dict]:
+    required = {
+        "C_TJENESTENR": st.text(),
+        "C_OBJECTGUID_I_AD": st.uuids().map(str),
+        "C_LOGIN": st.text(),
+        "EMAIL": st.text(),
+        "C_DIREKTE_TLF": st.text(),
+        "CELLPHONE": st.text(),
+        "C_INST_PHONE": st.text(),
     }
-    respx_mock.get(settings.odata_url).respond(json=data)
+    optional = {
+        "Id": st.integers(),
+        "UId": st.text(),
+        "EMAIL2": st.text(),
+    }
+    return draw(st.lists(st.fixed_dictionaries(required, optional=optional)))
 
 
 @pytest.mark.asyncio
-async def test_get_it_users(mock_omada_it_users: None) -> None:
-    expected = [
-        OmadaITUser(
-            C_TJENESTENR="00185",
-            C_OBJECTGUID_I_AD="9ff2beb8-2283-410f-babc-70d3ec2afa0c",
-            EMAIL="KristofferMoller.Jensen@silkeborg.dk",
-            C_DIREKTE_TLF="",
-            CELLPHONE="",
-            C_INST_PHONE="",
-            C_LOGIN="",
-        ),
-        OmadaITUser(
-            C_TJENESTENR="00187",
-            C_OBJECTGUID_I_AD="1106d3a6-a624-43b5-bdb8-66b746a2a60d",
-            EMAIL="Gitte.Willumsen@silkeborg.dk",
-            C_DIREKTE_TLF="89701891",
-            CELLPHONE="25306035",
-            C_INST_PHONE="89701000",
-            C_LOGIN="DR00187",
-        ),
-    ]
-    actual = await omada.get_it_users(settings.odata_url)
-
-    assert actual == expected
+@given(it_users())
+async def test_get_it_users(it_users: list[dict]) -> None:
+    with respx.mock as respx_mock:
+        respx_mock.get(settings.odata_url).respond(json={"value": it_users})
+        await omada.get_it_users(settings.odata_url)
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,15 +1,28 @@
 # SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
-from datetime import datetime
+from datetime import date
 from uuid import uuid4
 
+from hypothesis import given
+from hypothesis import strategies as st
+from hypothesis.strategies import composite
 from ramodels.mo import MOBase
 from ramodels.mo._shared import Validity
 
 from os2mint_omada.util import as_terminated
 
 
-def test_as_terminated() -> None:
+@composite
+def from_and_to_date(draw) -> tuple[date, date]:
+    from_date = draw(st.dates())
+    to_date = draw(st.dates(min_value=from_date))
+    return from_date, to_date
+
+
+@given(from_and_to_date())
+def test_as_terminated(from_and_to_date: tuple[date, date]) -> None:
+    from_date, to_date = from_and_to_date
+
     class Model(MOBase):
         validity: Validity
 
@@ -17,16 +30,16 @@ def test_as_terminated() -> None:
     obj = Model(
         uuid=uuid,
         validity=Validity(
-            from_date=datetime(2010, 1, 2),
+            from_date=from_date,
             to_date=None,
         ),
     )
-    actual = as_terminated(obj, from_date=datetime(2011, 2, 3))
+    actual = as_terminated(obj, from_date=from_date)
     expected = Model(
         uuid=uuid,
         validity=Validity(
-            from_date=datetime(2010, 1, 2),
-            to_date=datetime(2011, 2, 3),
+            from_date=from_date,
+            to_date=from_date,
         ),
     )
     assert actual == expected
