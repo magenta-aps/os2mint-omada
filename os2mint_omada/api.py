1988a5bf59b1a2af41eeec47d6ee65080d94efc1
Andreas Kring <andreas@magenta-aps.dk>
2021-12-07 10:09:28 +0100

    API locking mechanism

--- os2mint_omada/api.py
+++ os2mint_omada/api.py
@@ -1,8 +1,11 @@
 # SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
+import asyncio
 from typing import Any
 
 from fastapi import APIRouter
+from fastapi import Response
+from starlette.status import HTTP_429_TOO_MANY_REQUESTS
 
 from os2mint_omada import mo
 from os2mint_omada import omada
@@ -11,52 +14,59 @@ from os2mint_omada.clients import model_client
 from os2mint_omada.config import settings
 
 router = APIRouter()
+lock = asyncio.Lock()
 
 
 @router.post("/import/it-users")
-async def import_it_users() -> dict[str, Any]:
+async def import_it_users(response: Response) -> dict[str, Any]:
     """
     Import Omada IT users into MO.
 
     Returns: Dictionary of statistics.
     """
-    # Get user information from MO and Omada
-    root_org_uuid = await mo.get_root_org()
-    it_system_uuid = await mo.get_it_system_uuid(
-        organisation_uuid=root_org_uuid,
-        user_key=settings.it_system_user_key,
-    )
 
-    address_classes = await mo.get_classes(
-        organisation_uuid=root_org_uuid,
-        facet="employee_address_type",
-    )
-    visibility_classes = await mo.get_classes(
-        organisation_uuid=root_org_uuid, facet="visibility"
-    )
-    mo_it_bindings = await mo.get_it_bindings(it_system_uuid)
-    mo_user_addresses = await mo.get_user_addresses()
-    mo_engagements = await mo.get_engagements()
-    omada_it_users = await omada.get_it_users(settings.odata_url)
+    if lock.locked():
+        response.status_code = HTTP_429_TOO_MANY_REQUESTS
+        return {"msg": "Already running"}
 
-    # Synchronise updated objects to MO
-    updated_objects = list(
-        sync.get_updated_mo_objects(
-            mo_it_bindings=mo_it_bindings,
-            omada_it_users=omada_it_users,
-            mo_user_addresses=mo_user_addresses,
-            mo_engagements=mo_engagements,
-            address_class_uuids=address_classes,
-            it_system_uuid=it_system_uuid,
-            address_visibility_uuid=visibility_classes["Intern"],
+    async with lock:
+        # Get user information from MO and Omada
+        root_org_uuid = await mo.get_root_org()
+        it_system_uuid = await mo.get_it_system_uuid(
+            organisation_uuid=root_org_uuid,
+            user_key=settings.it_system_user_key,
         )
-    )
-    async with model_client.context():
-        await model_client.load_mo_objs(updated_objects)
 
-    return dict(
-        num_updated_objects=len(updated_objects),
-    )
+        address_classes = await mo.get_classes(
+            organisation_uuid=root_org_uuid,
+            facet="employee_address_type",
+        )
+        visibility_classes = await mo.get_classes(
+            organisation_uuid=root_org_uuid, facet="visibility"
+        )
+        mo_it_bindings = await mo.get_it_bindings(it_system_uuid)
+        mo_user_addresses = await mo.get_user_addresses()
+        mo_engagements = await mo.get_engagements()
+        omada_it_users = await omada.get_it_users(settings.odata_url)
+
+        # Synchronise updated objects to MO
+        updated_objects = list(
+            sync.get_updated_mo_objects(
+                mo_it_bindings=mo_it_bindings,
+                omada_it_users=omada_it_users,
+                mo_user_addresses=mo_user_addresses,
+                mo_engagements=mo_engagements,
+                address_class_uuids=address_classes,
+                it_system_uuid=it_system_uuid,
+                address_visibility_uuid=visibility_classes["Intern"],
+            )
+        )
+        async with model_client.context():
+            await model_client.load_mo_objs(updated_objects)
+
+        return dict(
+            num_updated_objects=len(updated_objects),
+        )
 
 
 @router.get("/ready")
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,10 @@
 # SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
+from unittest.mock import patch
+
 from fastapi.testclient import TestClient
 from starlette.status import HTTP_200_OK
+from starlette.status import HTTP_429_TOO_MANY_REQUESTS
 
 from os2mint_omada.main import app
 
@@ -12,3 +15,11 @@ client = TestClient(app)
 def test_readiness() -> None:
     r = client.get("/ready")
     assert HTTP_200_OK == r.status_code
+
+
+def test_lock() -> None:
+    with patch("os2mint_omada.api.lock.locked") as mock_locked:
+        mock_locked.return_value = True
+        r = client.post("/import/it-users")
+        assert HTTP_429_TOO_MANY_REQUESTS == r.status_code
+        assert r.json() == {"msg": "Already running"}
