a343571ab5a553678905765f09a127f46c73f02b
Casper V. Kristensen <vk@magenta.dk>
2024-04-30 17:43:31 +0200

    test(egedal): [#59294] user refresh

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -4,10 +4,13 @@ from collections.abc import AsyncIterator
 from collections.abc import Callable
 from collections.abc import Iterator
 from pathlib import Path
+from typing import Awaitable
 
 import pytest
 from fastapi.testclient import TestClient
+from fastramqpi.ramqp.depends import rate_limit
 from gql.client import AsyncClientSession
+from httpx import AsyncClient
 from pytest import MonkeyPatch
 from respx import ASGIHandler
 from respx import MockRouter
@@ -34,10 +37,14 @@ def omada_mock(respx_mock: MockRouter) -> Callable[[list], None]:
 def test_client(monkeypatch: MonkeyPatch, tmp_path: Path) -> Iterator[TestClient]:
     """Create ASGI test client with associated lifecycles."""
     monkeypatch.setenv("OMADA__URL", "http://fake-omada-api/")
-    monkeypatch.setenv("OMADA__INTERVAL", "2")
+    monkeypatch.setenv("OMADA__INTERVAL", "1")
     monkeypatch.setenv("OMADA__PERSISTENCE_FILE", str(tmp_path / "omada.json"))
 
     app = create_app()
+
+    # Decrease rate-limit to avoid tests (and developers) timing out
+    app.dependency_overrides[rate_limit()] = rate_limit(1)
+
     with TestClient(app) as client:
         yield client
 
@@ -46,3 +53,19 @@ def test_client(monkeypatch: MonkeyPatch, tmp_path: Path) -> Iterator[TestClient
 async def graphql_client(test_client: TestClient) -> AsyncIterator[AsyncClientSession]:
     """Authenticated GraphQL codegen client for OS2mo."""
     yield test_client.app_state["context"]["graphql_client"]
+
+
+@pytest.fixture
+def get_num_queued_messages(
+    rabbitmq_management_client: AsyncClient,
+) -> Callable[[], Awaitable[int]]:
+    """Get number of queued messages in RabbitMQ AMQP."""
+
+    async def _get_num_queued_messages() -> int:
+        queues = (await rabbitmq_management_client.get("queues")).json()
+        return sum(
+            queue.get("messages_ready", 0) + queue.get("messages_unacknowledged", 0)
+            for queue in queues
+        )
+
+    return _get_num_queued_messages
