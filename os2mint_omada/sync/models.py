606ae47c93c6c8846cb29278f722e52c95a9179c
Casper V. Kristensen <casper@vkristensen.dk>
2024-09-06 17:59:24 +0200

    refactor: [#59335] eliminate RA ITUser

--- os2mint_omada/sync/models.py
+++ os2mint_omada/sync/models.py
@@ -9,6 +9,7 @@ from uuid import UUID
 
 import structlog
 from pydantic import BaseModel
+from pydantic import Extra
 from pydantic import validator
 from ramodels.mo import Validity as RAValidity
 
@@ -56,6 +57,62 @@ class RAStripUserKeyMixin(BaseModel):
         if absent).
         """
         return None
+
+
+class StrictBaseModel(BaseModel):
+    """Pydantic BaseModel with strict(er) defaults."""
+
+    class Config:
+        extra = Extra.forbid
+        frozen = True
+
+
+class StripUUIDMixin(StrictBaseModel):
+    @validator("uuid", check_fields=False)
+    def strip_uuid(cls, uuid: UUID | None) -> None:
+        """Strip UUID to allow for convenient comparison of models."""
+        return None
+
+
+class StripUserKeyMixin(StrictBaseModel):
+    @validator("user_key", check_fields=False)
+    def strip_user_key(cls, user_key: Any | None) -> None:
+        """Strip user key to allow for convenient comparison of models.
+
+        Should only be used on objects where the user key does not contain actual
+        relevant data, but is simply a copy of the UUID (as automatically set by MOBase
+        if absent).
+        """
+        return None
+
+
+class Validity(StrictBaseModel):
+    start: datetime
+    end: datetime | None
+
+    @validator("start", "end")
+    def validity_at_midnight(cls, value: datetime | None) -> datetime | None:
+        """Normalise validity dates to allow for convenient comparison of models.
+
+        Date(time)s are converted to "midnight" for MO compatibility by removing the
+        time-component.
+        """
+        if value is None:
+            return None
+        return datetime.combine(value, time.min)
+
+
+class ITUser(StrictBaseModel):
+    uuid: UUID | None
+    user_key: str | None
+    it_system: UUID
+    person: UUID
+    engagement: UUID | None
+    validity: Validity
+
+
+class ComparableITUser(StripUUIDMixin, ITUser):
+    pass
 
 
 CPR_ONLY_NORMAL_REGEX = r"(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[0-2])(\d{2})(\d{4})"
