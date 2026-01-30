8b7cf0368d7ef79c074f68160ddf477e8159cb83
Casper V. Kristensen <casper@vkristensen.dk>
2026-01-30 15:25:41 +0100

    feat(silkeborg): [#66701] use `C_OS2MO_ID` to determine if manual

--- os2mint_omada/sync/silkeborg/models.py
+++ os2mint_omada/sync/silkeborg/models.py
@@ -7,6 +7,24 @@ from pydantic import Field
 from os2mint_omada.omada.models import OmadaUser
 from os2mint_omada.sync.models import CPR_INCL_FICTIVE_REGEX
 
+# All users have a C_TJENESTENR, and SD users additionally have a C_OS2MO_ID. A
+# user is a 'manual' user if it isn't an SD user.
+#
+# | C_TJENESTENR | C_OS2MO_ID | count | manual? |
+# |--------------|------------|-------|---------|
+# | Yes          | Yes        | 7000  | No      |
+# | Yes          | No         | 600   | Yes     |
+#
+# All users have addresses and IT-users synchronised. Manual users also have
+# their employee object and associated engagements synchronised from Omada.
+#
+# C_TJENESTENR and C_OS2MO_ID are similar, and it is possible to calculate one
+# from the other. An example:
+#
+#   C_TJENESTENR: 1000F
+#   C_OS2MO_ID: TF-10005
+#
+
 
 class SilkeborgOmadaUser(OmadaUser):
     """Silkeborg-specific Omada user model."""
@@ -21,6 +39,7 @@ class SilkeborgOmadaUser(OmadaUser):
 
     # Engagement
     service_number: str = Field(alias="C_TJENESTENR")
+    os2mo_id: str | None = Field(alias="C_OS2MO_ID")
 
     # IT User
     ad_guid: UUID = Field(alias="C_OBJECTGUID_I_AD")
@@ -33,17 +52,13 @@ class SilkeborgOmadaUser(OmadaUser):
     phone_institution: str | None = Field(alias="C_INST_PHONE")
 
     @property
-    def is_manual(self):
-        """Manually created users have IdentityCategory ID 561 in Silkeborg"""
-        return self.identity_category.id == "561"
+    def is_manual(self) -> bool:
+        """Manual users don't have a C_OS2MO_ID ID."""
+        return self.os2mo_id is None
 
 
 class ManualSilkeborgOmadaUser(SilkeborgOmadaUser):
-    """Silkeborg-specific Omada user with additional fields for 'manual' users.
-
-    Manual users not only have addresses and IT users synchronised, but also the
-    employee object itself, as well as associated engagements.
-    """
+    """Silkeborg-specific Omada user with additional fields for 'manual' users."""
 
     # Employee
     first_name: str = Field(alias="C_FORNAVNE")
