74e97b29211b839fa642bd838efb2a161e736079
Casper V. Kristensen <casper@vkristensen.dk>
2026-09-11 14:49:57 +0200

    fix: [#71500] remove support for http basic auth

--- os2mint_omada/config.py
+++ os2mint_omada/config.py
@@ -18,11 +18,6 @@ class OmadaOIDCSettings(BaseModel):
     scope: str
 
 
-class OmadaBasicAuthSettings(BaseModel):
-    username: str
-    password: str
-
-
 class OmadaAMQPConnectionSettings(AMQPConnectionSettings):
     exchange = "omada"
     queue_prefix = "omada"
@@ -32,7 +27,6 @@ class OmadaSettings(BaseModel):
     # OData view: http://omada.example.org/OData/DataObjects/Identity?viewid=xxxxx
     url: AnyHttpUrl
     oidc: OmadaOIDCSettings | None = None
-    basic_auth: OmadaBasicAuthSettings | None = None
 
     amqp: OmadaAMQPConnectionSettings
     interval: int = 600
